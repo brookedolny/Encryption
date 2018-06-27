1abd4a208bd10ede0bb7ca36623258d544f6ff5c
Brooke Dolny <brookedolny@gmail.com>
2018-06-26 22:01:55 -0400

    Update cbc.c

--- AES/cbc.c
+++ AES/cbc.c
@@ -6,11 +6,6 @@ uint8_t tempInitVector[16];
 
 
 void generateInitVector(void) {
-    // NOTE: this implementation of CBC is technically invalid.
-    // the random number generation for the initalization vector is not
-    // uniformly distributed, and uses an LCG which can be implemented
-    // in a way that generates predictable random numbers. Also,
-    // using the time as a the seed can cause obvious problems.
     srand(time(NULL));
     for(int i = 0; i < 16; i++) {
         tempInitVector[i] = (uint8_t) (rand() % 256);
