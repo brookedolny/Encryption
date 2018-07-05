582412f9876e7e9d008a557980b4687ce52bab2f
Brooke Dolny <brookedolny@gmail.com>
2018-07-04 22:05:43 -0400

    Added Documentation

--- AES/cbc.h
+++ AES/cbc.h
@@ -32,6 +32,22 @@ void encryptBlock(uint8_t * previous, uint8_t * text, uint8_t words[][4], int Nk
  */
 void decryptBlock(uint8_t * previous, uint8_t * text, uint8_t words[][4], int Nk, int Nb, int Nr);
 
+/**
+ * Encrypts a file using the cbc mode of encryption
+ *
+ * @param plaintextStream the file stream to read the plaintext from
+ * @param ciphertextStream the file stream to write the ciphertext to
+ * @param key the key to encrypt the file
+ * @param type the flavour of AES to encrypt the document with
+ */
 void cbcEncryptFile(FILE * plaintextStream, FILE * ciphertextStream, uint8_t * key, int type);
 
+/**
+ * Decrypts a file using the cbc mode of encryption
+ *
+ * @param plaintextStream the file stream to write the plaintext to
+ * @param ciphertextStream the file stream to read the ciphertext from
+ * @param key the key to decrypt the file
+ * @param type the flavour of AES to decrypt the document with
+ */
 void cbcDecryptFile(FILE * plaintextStream, FILE * ciphertextStream, uint8_t * key, int type);
