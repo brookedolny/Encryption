ae875ce8d45acdcd3e43742591eabe54fe2b4050
Brooke Dolny <brookedolny@gmail.com>
2018-07-25 20:15:48 -0400

    Added SHA operation functions
    Created a SHA functions file, containing the functions defined in the
    SHA standard.

---
+++ SHA/sha_functions.c
@@ -0,0 +1,41 @@
+#include "sha_functions.h"
+
+uint32_t rotateLeft32(uint32_t x, int n) {
+    return (x << n) | (x >> (32 - n));
+}
+
+uint64_t rotateLeft64(uint64_t x, int n) {
+    return (x << n) | (x >> (64 - n));
+}
+
+uint32_t rotateRight32(uint32_t x, int n) {
+    return (x >> n) | (x << (32 - n));
+}
+
+uint64_t rotateRight64(uint64_t x, int n) {
+    return (x >> n) | (x << (64 - n));
+}
+
+uint32_t choose32(uint32_t x, uint32_t y, uint32_t z){
+    return (x & y) ^ (!x & z);
+}
+
+uint64_t choose64(uint64_t x, uint64_t y, uint64_t z){
+    return (x & y) ^ (!x & z);
+}
+
+uint32_t parity32(uint32_t x, uint32_t y, uint32_t z) {
+    return x ^ y ^ z;
+}
+
+uint64_t parity64(uint64_t x, uint64_t y, uint64_t z) {
+    return x ^ y ^ z;
+}
+
+uint32_t majority32(uint32_t x, uint32_t y, uint32_t z){
+    return (x & y) ^ (x & z) ^ (y & z);
+}
+
+uint64_t majority64(uint64_t x, uint64_t y, uint64_t z){
+    return (x & y) ^ (x & z) ^ (y & z);
+}
