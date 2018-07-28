b96acc8c5c56474431e4332ce7782b6020754fb9
Brooke Dolny <brookedolny@gmail.com>
2018-07-28 00:23:54 -0400

    Sad Bugs part 2

--- SHA/sha_functions.h
+++ SHA/sha_functions.h
@@ -3,7 +3,7 @@
 
 #define rotateLeft(x, n, w) (((x) << (n)) | ((x) >> ((w) - (n))))
 #define rotateRight(x, n, w) (((x) >> (n)) | ((x) << ((w) - (n))))
-#define choose(x, y, z) (((x) & (y)) ^ (!(x) & (z)))
+#define choose(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
 #define parity(x, y, z) ((x) ^ (y) ^ (z))
 #define majority(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
 
