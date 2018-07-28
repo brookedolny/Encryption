7071a4ce626556742b023cc7348c3874f139bf09
Brooke Dolny <brookedolny@gmail.com>
2018-07-27 21:12:01 -0400

    Sad bugs
    thanks @Wesley for pointing out my missing brackets
    code didn't even compile HAHA

--- SHA/sha_functions.h
+++ SHA/sha_functions.h
@@ -1,14 +1,10 @@
 #ifndef SHA_FUNCTIONS_H
 #define SHA_FUNCTIONS_H
 
-#define rotateLeft(x, n, w) ((x << n) | (x >> (w - n)))
-
-#define rotateRight(x, n, w) ((x >> n) | (x << (w - n)))
-
-#define choose(x, y, z) ((x & y) ^ (!x & z))
-
-#define parity(x, y, z) (x ^ y ^ z)
-
-#define majority(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
+#define rotateLeft(x, n, w) (((x) << (n)) | ((x) >> ((w) - (n))))
+#define rotateRight(x, n, w) (((x) >> (n)) | ((x) << ((w) - (n))))
+#define choose(x, y, z) (((x) & (y)) ^ (!(x) & (z)))
+#define parity(x, y, z) ((x) ^ (y) ^ (z))
+#define majority(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
 
 #endif
