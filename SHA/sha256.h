eea327d944275558335a80612c82adc74e7abbc8
Brooke Dolny <brookedolny@gmail.com>
2018-08-07 21:15:16 -0400

    Added sha256 functions to header file

--- SHA/sha256.h
+++ SHA/sha256.h
@@ -4,6 +4,18 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+uint32_t S_0_256(uint32_t x);
 
+uint32_t S_1_256(uint32_t x);
+
+uint32_t s_0_256(uint32_t x);
+
+uint32_t s_1_256(uint32_t x);
+
+void initalSHA224HashValue(uint32_t * hash);
+
+void initalSHA256HashValue(uint32_t * hash);
+
+void SHA256MessageSchedule(uint32_t * message, uint32_t * words);
 
 #endif
