22a94644bb5d1c8d839e75ba6795131d7f5c1d3f
Brooke Dolny <brookedolny@gmail.com>
2018-08-12 19:48:47 -0400

    Use generic filestreams for hash input

--- sha/sha.c
+++ sha/sha.c
@@ -1,30 +1,29 @@
 #include "sha.h"
 #include "sha1.h"
 #include "sha256.h"
-#include <stdio.h>
 
 void SHA1init(uint32_t * hash) {
     initalSHA1HashValue(hash);
 }
 
-void SHA1(uint32_t * hash) {
+void SHA1(FILE * input, uint32_t * hash) {
     uint8_t message[64] = {0};
     uint32_t parsed[16] = {0};
     size_t elements;
     size_t length = 0;
-    elements = fread(message, 1, 64, stdin);
+    elements = fread(message, 1, 64, input);
     length += elements;
     while(elements == 64) {
         SHA1Parsing(message, parsed);
         SHA1Iteration(parsed, hash);
-        elements = fread(message, 1, 64, stdin);
+        elements = fread(message, 1, 64, input);
         length += elements;
     }
-    if (feof(stdin)) {
+    if (feof(input)) {
         SHA1Padding(message, length * 8);
         SHA1Parsing(message, parsed);
         SHA1Iteration(parsed, hash);
-    } else if (ferror(stdin)) {
+    } else if (ferror(input)) {
         perror("Failed to compute SHA1 hash");
     }
 }
@@ -33,33 +32,33 @@ void SHA224init(uint32_t * hash) {
     initalSHA224HashValue(hash);
 }
 
-void SHA224(uint32_t * hash) {
-    SHA256(hash);
-    hash[8] = 0;
+void SHA224(FILE * input, uint32_t * hash) {
+    SHA256(input, hash);
+    hash[7] = 0;
 }
 
 void SHA256init(uint32_t * hash) {
     initalSHA256HashValue(hash);
 }
 
-void SHA256(uint32_t * hash) {
+void SHA256(FILE * input, uint32_t * hash) {
     uint8_t message[64] = {0};
     uint32_t parsed[16] = {0};
     size_t elements;
     size_t length = 0;
-    elements = fread(message, 1, 64, stdin);
+    elements = fread(message, 1, 64, input);
     length += elements;
     while(elements == 64) {
         SHA1Parsing(message, parsed);
         SHA256Iteration(parsed, hash);
-        elements = fread(message, 1, 64, stdin);
+        elements = fread(message, 1, 64, input);
         length += elements;
     }
-    if (feof(stdin)) {
+    if (feof(input)) {
         SHA1Padding(message, length * 8);
         SHA1Parsing(message, parsed);
         SHA256Iteration(parsed, hash);
-    } else if (ferror(stdin)) {
+    } else if (ferror(input)) {
         perror("Failed to compute SHA1 hash");
     }
 }
--- sha/sha.h
+++ sha/sha.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 extern uint64_t K_256[64];
 extern uint64_t K_512[80];
@@ -13,14 +14,14 @@ void SHA1init(uint32_t * hash);
  * Computes the SHA1 hash from a message from stdin
  * @param hash the location to store the final SHA1 hash
  */
-void SHA1(uint32_t * hash);
+void SHA1(FILE * input, uint32_t * hash);
 
 void SHA224init(uint32_t * hash);
 
-void SHA224(uint32_t * hash);
+void SHA224(FILE * input, uint32_t * hash);
 
 void SHA256init(uint32_t * hash);
 
-void SHA256(uint32_t * hash);
+void SHA256(FILE * input, uint32_t * hash);
 
 #endif
