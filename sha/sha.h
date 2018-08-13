b264a2f7b2a09eddcd4056d7f280b41de774df9a
Brooke Dolny <brookedolny@gmail.com>
2018-08-13 18:29:59 -0400

    Updated SHA2 documentation

--- sha/sha.h
+++ sha/sha.h
@@ -7,20 +7,43 @@
 
 extern uint64_t K_512[80];
 
+/**
+ * Initalizes the hash for SHA1
+ * @param hash the hash to be initalized
+ */
 void SHA1init(uint32_t * hash);
 
 /**
  * Computes the SHA1 hash from a message from stdin
+ * @param input the input filestream
  * @param hash the location to store the final SHA1 hash
  */
 void SHA1(FILE * input, uint32_t * hash);
 
+/**
+ * Initalizes the hash for SHA224
+ * @param hash the hash to be initalized
+ */
 void SHA224init(uint32_t * hash);
 
+/**
+ * Computes the SHA224 hash from a message from stdin
+ * @param input the input filestream
+ * @param hash the location to store the final SHA224 hash
+ */
 void SHA224(FILE * input, uint32_t * hash);
 
+/**
+ * Initalizes the hash for SHA256
+ * @param hash the hash to be initalized
+ */
 void SHA256init(uint32_t * hash);
 
+/**
+ * Computes the SHA1 hash from a message from stdin
+ * @param input the input filestream
+ * @param hash the location to store the final SHA1 hash
+ */
 void SHA256(FILE * input, uint32_t * hash);
 
 #endif
